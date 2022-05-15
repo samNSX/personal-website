e5246d543604ac5a0305ee084d8b19fc5943c9c2
Caleb Taylor <aquaductape@gmail.com>
2022-05-15 05:57:42 -0700

    update

--- src/components/IntroProfile.jsx
+++ src/components/IntroProfile.jsx
@@ -1,17 +1,38 @@
+import { onMount } from "solid-js";
+
 const IntroProfile = () => {
+  let img1;
+  let img2;
+  onMount(() => {
+    const backgroundImage = `url(${profileImg})`;
+
+    if (!img1) {
+      setTimeout(() => {
+        if (!img1) return;
+        img1.style.backgroundImage = backgroundImage;
+        img2.style.backgroundImage = backgroundImage;
+      }, 100);
+      return;
+    }
+
+    img1.style.backgroundImage = backgroundImage;
+    img2.style.backgroundImage = backgroundImage;
+  });
   return (
     <div className="relative">
       <div
         className="w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] relative z-1"
-        style={`background-image: url(${profileImg});    background-size: contain;
+        style={`background-size: contain;
         background-position: center;
         background-repeat: no-repeat;`}
+        ref={img1}
       ></div>
       <div
         className="absolute top-0 left-0 w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] blur-[23px] translate-x-[12px] translate-y-[24px]"
-        style={`background-image: url(${profileImg});    background-size: contain;
+        style={`background-size: contain;
         background-position: center;
         background-repeat: no-repeat;`}
+        ref={img2}
       ></div>
       {/* <img
               className="w-[200px] sm:w-[250px] relative z-1"
