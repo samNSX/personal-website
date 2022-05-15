21ca5e3df937565fe49b5b9e37b179733f765432
Caleb Taylor <aquaductape@gmail.com>
2022-05-15 05:45:38 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -40,21 +40,11 @@ function App() {
 
   return (
     <div className="">
-      {/* Header */}
       <header
         className="fixed top-0 left-0 w-full  has-backdrop-filter:backdrop-blur-lg z-50 has-backdrop-filter:bg-[#e7f3ff]/60 bg-[#e7f3ff]/95"
         style="box-shadow: 0 0 13px 0px #0b487700; transition: box-shadow 300ms;"
         ref={headerEl}
       >
-        {/* <div className="flex justify-between items-center h-[100px] max-w-[1000px] mx-auto px-5">
-          <div>
-            <span className="text-xl font-semibold">Sam Lu</span> /{" "}
-            <span className="text-lg text-gray-500">
-              GigaByte Technical Support
-              <span className="hidden lg:inline"> Representative</span>
-            </span>
-          </div>
-        </div> */}
         <div
           className="absolute top-0 right-0 bottom-0 w-8 pointer-events-none"
           style="background: linear-gradient(to left, #e6f3ff 5px, transparent)"
@@ -69,6 +59,9 @@ function App() {
               return (
                 <button
                   className=" border-solid border-b-2 border-transparent flex-shrink-0 transition-all hover:border-blue-700 font-bold"
+                  classList={{
+                    "border-blue-700": item.link === activeLink().link,
+                  }}
                   onClick={() => onClick(item.link)}
                 >
                   {item.link}
@@ -79,34 +72,8 @@ function App() {
         </div>
       </header>
 
-      <main className="px-6 max-w-[800px] mx-auto text-xl overflow-hidden">
+      <main className="px-6 max-w-[800px] mx-auto text-xl">
         {activeLink().component}
-        {/* <section className="">
-          <div className="px-5">
-            <h1 className="font-bold text-6xl mb-5 lg:whitespace-nowrap text-center">
-              Hello, I'm Samuel Lu
-              <div className="text-xl my-3 text-gray-500">
-                GigaByte Technical Support Representative
-              </div>
-            </h1>
-            <div className="flex flex-col items-center justify-center">
-              <p className="text-xl">
-                To the person reading this, my name is Samuel Lu and I would
-                like thank you for taking time out of your day to arrive at my
-                page. Here you can learn more about me, see what projects I have
-                been a part of, my experiences, any certificates or licenses I
-                obtained, and finally contact information.
-              </p>
-            </div>
-          </div>
-          <div className="">
-            <div className="flex justify-center pb-16">
-              <div className="relative">
-                <img className="w-[300px]" src={profileColorfullImg} alt="" />
-              </div>
-            </div>
-          </div>
-        </section> */}
       </main>
       {/* <footer className="mt-auto p-5">
         <p>© 2023 by Samuel Lu.</p>
--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -1,22 +1,11 @@
 import profileColorfullImg from "./assets/sam_profile.png";
+import IntroProfile from "./components/IntroProfile";
 const Introduction = () => {
   return (
     <div className="flex justify-center sm:items-center pt-24 sm:pt-0 sm:h-screen">
       <div className="px-5">
         <div className="flex justify-center pb-2 blurIn opacity-0 blur-0">
-          <div className="relative">
-            <img
-              className="w-[200px] sm:w-[250px] relative z-1"
-              src={profileColorfullImg}
-              alt=""
-            />
-
-            <img
-              className="absolute top-0 left-0 w-[200px] sm:w-[250px] blur-[23px] translate-x-[12px] translate-y-[24px]"
-              src={profileColorfullImg}
-              alt=""
-            />
-          </div>
+          <IntroProfile></IntroProfile>
         </div>
         <h1 className="font-bold text-[43px] sm:text-6xl leading-[1em] mb-5 lg:whitespace-nowrap text-center">
           <span className="block fadeInLeft">
@@ -24,7 +13,7 @@ const Introduction = () => {
             <span className="hidden sm:inline"> </span>
             <span className="block sm:inline">Samuel Lu</span>
           </span>
-          <div className="text-xl my-3 text-gray-500 fadeInRight">
+          <div className="text-xl my-3 text-gray-500 fadeInRight overflow-hidden sm:overflow-visible">
             GigaByte Technical Support Representative
           </div>
         </h1>
